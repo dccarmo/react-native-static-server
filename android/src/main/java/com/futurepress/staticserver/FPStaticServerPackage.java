23d9085e8b197fc5e539931ad31d37f92acc1a59
Diogo <diogo@MacBook-Pro.local>
2017-12-26 19:05:31 -0200

    Updated server to show external IP

--- android/src/main/java/com/futurepress/staticserver/FPStaticServerPackage.java
+++ android/src/main/java/com/futurepress/staticserver/FPStaticServerPackage.java
@@ -1,28 +1,72 @@
 
 package com.futurepress.staticserver;
 
-import java.util.*;
-
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
-public class FPStaticServerPackage implements ReactPackage {
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.LifecycleEventListener;
+import com.facebook.react.bridge.ReadableMap;
 
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-        modules.add(new FPStaticServerModule(reactContext));
-        return modules;
-    }
+import java.io.IOException;
 
-   // Deprecated RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
+public class FPStaticServerModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
+  private WebServer server = null;
 
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+  public FPStaticServerModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  public String getName() {
+    return "FPStaticServer";
+  }
+
+  @Override
+  public void onHostResume() {
+    try {
+      server.start();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
+
+  @Override
+  public void onHostPause() {
+    stop();
+  }
+
+  @Override
+  public void onHostDestroy() {
+    stop();
+  }
+
+  @ReactMethod
+  public void start(ReadableMap options, Promise promise) {
+    try {
+      if (server != null && server.wasStarted()) {
+        server.stop();
+      }
+
+      server = new WebServer(options.getInt("port"));
+      server.start();
+
+      promise.resolve("http://" + server.getHostname() + ":" + server.getListeningPort());
+
+    } catch (IOException e) {
+      promise.reject(null, e.getMessage());
+    }
+  }
+
+  @ReactMethod
+  public void setHtml(String html) {
+    server.html = html;
+  }
+
+  @ReactMethod
+  public void stop() {
+    if (server.wasStarted()) {
+      server.stop();
+    }
+  }
 }
--- android/src/main/java/com/futurepress/staticserver/WebServer.java
+++ android/src/main/java/com/futurepress/staticserver/WebServer.java
@@ -1,14 +1,42 @@
 package com.futurepress.staticserver;
 
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 import fi.iki.elonen.NanoHTTPD;
 
 public class WebServer extends NanoHTTPD {
   public String html = "";
 
-  public WebServer(String hostname, int port) throws IOException {
-    super(hostname, port);
+
+  public WebServer(int port) throws IOException {
+    super(port);
+  }
+
+  @Override
+  public String getHostname() {
+    return getLocalIpAddress();
+  }
+
+  public static String getLocalIpAddress() {
+    try {
+      for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+        NetworkInterface intf = en.nextElement();
+        for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+          InetAddress inetAddress = enumIpAddr.nextElement();
+          if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+            return inetAddress.getHostAddress();
+          }
+        }
+      }
+    } catch (SocketException ex) {
+      ex.printStackTrace();
+    }
+    return null;
   }
 
   @Override
